2a872d813c623b7d0386f0d473c2c939fcfa05d1
Shane Gill <tothegills@gmail.com>
2015-08-03 15:02:53 +1000

    Move to vs2013

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -2,9 +2,9 @@
 
 namespace HelloWorld
 {
-    public class Program
+    class Program
     {
-        public void Main(string[] args)
+        static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
         }
